09e938dfb88f5e039254374247b15f668eb2b902
“jriverar” <jriverarosero@gmail.com>
2025-04-04 09:21:40 -0500

    Se completo la adición de la tarea en el DOM

--- js/app.js
+++ js/app.js
@@ -1,10 +1,41 @@
 document.addEventListener('DOMContentLoaded', () => {
     const taskForm = document.getElementById('task-form');
     const taskInput = document.getElementById('task-input');
+    const taskList = document.getElementById('task-list');
 
-    taskForm.addEventListener('submit', (e) => {
-        var vti = taskInput.value.trim();
-        alert("Se envió el formulario: " + vti);
+    let tasks = [];
+    taskForm.addEventListener('click', (e) => {
+        const vti = taskInput.value.trim();
+        if (vti !== '') {
+            const task = {
+                id: Date.now,
+                text: vti,
+                complete: false
+            };
+            tasks.push(task);
+            console.log(tasks);
+            renderTasks();
+            taskInput.value = '';
+        }
     });
+
+    function renderTasks() {
+        taskList.innerHTML = '';
+        tasks.forEach(
+            task => {
+                console.log(task);
+
+                const li = document.createElement('li');
+                li.innerHTML =
+                    '<span>' + task.text + '</span>' +
+                    '<div>' +
+                    '<button onclick="deleteTask(' + task.id + ')">' +
+                    'Eliminar </button>' +
+                    '</div>';
+                taskList.appendChild(li);
+            }
+
+        );
+    }
 
 });
