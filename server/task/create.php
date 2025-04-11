47010e3b22220a62fd3aca36f403633d2b51eacb
“jriverar” <jriverarosero@gmail.com>
2025-04-11 11:58:44 -0500

    Agregando carpetas faltantes

---
+++ server/task/create.php
@@ -0,0 +1,32 @@
+<?php
+require '../commons/db.php';
+
+var_dump($_SERVER['REQUEST_METHOD']);
+var_dump($_POST);
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (
+        trim($_POST['title']) != '' &&
+        trim($_POST['user_id']) != '' &&
+        trim($_POST['category_id']) != ''
+    ) {
+
+        $q = "INSERT INTO task.task(title, description, due_date, completed, user_id, category_id)";
+        $q = $q . " VALUES (:title, :description, :due_date, :completed, :user_id, :category_id );";
+        $stmt = $db->prepare($q);
+        $stmt->execute([
+            "title" => $_POST["title"],
+            "description" => $_POST["description"],
+            "due_date" => $_POST["due_date"],
+            "completed" => $_POST["completed"],
+            "user_id" => $_POST["user_id"],
+            "category_id" => $_POST["category_id"]
+        ]);
+
+        header("Location: /task-manager/");
+
+    } else {
+        echo 'Nooooooooooo pasa';
+    }
+}
+
+?>
